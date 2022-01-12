44a3ad8bc36bd121c17555ab36265c3da6c0ec36
Henrylle Maia <henryllemaia@gmail.com>
2022-01-11 21:22:47 -0300

    ajuste copyright

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer>
-      <p>Copyright Oregon EAD 2021</p>
+      <p>Copyright Oregon EAD 2022</p>
       <Link to="/about">Sobre o BIA</Link>
     </footer>
   );

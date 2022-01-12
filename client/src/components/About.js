1f9d3b7d9e368f14ed4bb138f7987b62df87399e
Henrylle Maia <henryllemaia@gmail.com>
2022-01-11 22:17:38 -0300

    execução de fix audit

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const About = () => {
   return (
     <div>
-      <h4>Versão 1.0.0</h4>
+      <h4>Versão 1.0.1</h4>
       <h5>BIA 17 a 20 de Janeiro/2022</h5>
       <Link to="/">Voltar</Link>
     </div>

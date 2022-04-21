e6f353ccfb50a3ac28d94cb671a8518b13873f04
Henrylle <henryllemaia@gmail.com>
2022-04-21 14:11:00 -0300

    novo teste para setup de vs code e chrome

---
+++ scripts/setup_vscode+chrome.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Instalando VS CODE
+sudo snap install --classic code
+
+#Instalando Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# INSTALANDO UI
+sudo apt update -y && \
+sudo apt upgrade -y && \
+sudo apt install xfce4 xfce4-goodies -y 

86a66d273be8461f8519b36ee321c669bc774187
Henrylle <henryllemaia@gmail.com>
2022-04-24 16:35:00 -0300

    dando permissões na pasta

--- scripts/setup_ui_ubuntu.sh
+++ scripts/setup_ui_ubuntu.sh
@@ -6,3 +6,6 @@ sudo apt install xfce4 xfce4-goodies -y
 # INSTALANDO CHROME REMOTE DESKTOP
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && \
   sudo apt install ./chrome-remote-desktop_current_amd64.deb -y
+  
+# DANDO PERMISSÕES NA PASTA  
+sudo chmod 777 /home/ubuntu/.config/

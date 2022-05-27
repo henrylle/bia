5d6b9e71a31a1f1e918bdc2a4a464b2186908cec
Henrylle Maia <henryllemaia@gmail.com>
2022-05-26 21:19:02 -0300

    ajuste na mensagem

--- client/src/App.js
+++ client/src/App.js
@@ -100,7 +100,7 @@ function App() {
                   onToggle={toggleReminder}
                 />
               ) : (
-                "Nenhuma tarefa por aqui"
+                "Nenhuma tarefa por aqui ainda"
               )}
             </>
           )}

a4d32768be91a80062b899b113a123bb6f43d31b
Henrylle <henryllemaia@gmail.com>
2022-08-04 19:41:08 -0300

    ajuste npm install

--- rodar_app_local_windows.bat
+++ rodar_app_local_windows.bat
@@ -1,5 +1,5 @@
 docker-compose up -d database
-call npm install --location=global npm@latest --loglevel=error
+call npm install -g npm@latest --loglevel=error
 call npm install --loglevel=error
 call npm run build --prefix client --loglevel=error
 call npx sequelize db:migrate

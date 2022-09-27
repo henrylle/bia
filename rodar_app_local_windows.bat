ed17ada23abfb51aea55f299edf6051c8221852d
Henrylle <henryllemaia@gmail.com>
2022-09-27 12:33:26 -0300

    check error .bat

--- rodar_app_local_windows.bat
+++ rodar_app_local_windows.bat
@@ -1,4 +1,6 @@
+@echo off
 docker-compose up -d database
+if %errorlevel% neq 0 exit /b %errorlevel%
 call npm install -g npm@latest --loglevel=error
 call npm install --loglevel=error
 call npm run build --prefix client --loglevel=error

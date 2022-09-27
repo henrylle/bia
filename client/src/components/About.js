eb4d56e29e36593cc17a6a0c7dcb6f8b7aea4a54
Henrylle <henryllemaia@gmail.com>
2022-09-27 15:12:39 -0300

    ajustes em datas.

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,7 +3,7 @@ const About = () => {
   return (
     <div>
       <h4>Versão 1.0.3</h4>
-      <h5>BIA 08 a 14 de Agosto/2022</h5>
+      <h5>BIA 10 a 16 de Outubro/2022</h5>
       <Link to="/">Voltar</Link>
     </div>
   );

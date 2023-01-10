00f53598eb2d720afcfa1b3d3f3d0ea9175c313c
Henrylle <henryllemaia@gmail.com>
2023-01-10 16:34:00 -0300

    alterando versão do banco e do node

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const About = () => {
   return (
     <div>
-      <h4>Versão 1.0.3</h4>
+      <h4>Versão 1.1.3</h4>
       <h5>BIA 16 a 22 de Janeiro/2023</h5>
       <Link to="/">Voltar</Link>
     </div>
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer>
-      <p>Copyright Oregon EAD 2022</p>
+      <p>Copyright Oregon EAD 2023</p>
       <Link to="/about">Sobre o BIA</Link>
     </footer>
   );

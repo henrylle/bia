6df72ce2e55e64b4c8e6c5946725df5e8ddc9f6d
Henrylle <henryllemaia@gmail.com>
2023-04-04 10:39:44 -0300

    setup cloudshell

---
+++ scripts/setup_cloudshell_ssm.sh
@@ -0,0 +1,2 @@
+curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm" -o "session-manager-plugin.rpm"
+sudo yum install -y session-manager-plugin.rpm

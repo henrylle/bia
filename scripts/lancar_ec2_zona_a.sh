3deff626160c8d37b4574227a6b9902c421b72f5
Henrylle Maia <henryllemaia@gmail.com>
2023-04-05 21:37:27 -0300

    retornando subnet_id

--- scripts/lancar_ec2_zona_a.sh
+++ scripts/lancar_ec2_zona_a.sh
@@ -1,3 +1,5 @@
 vpc_id=$(aws ec2 describe-vpcs --filters Name=isDefault,Values=true --query "Vpcs[0].VpcId" --output text)
-echo $vpc_id
-#aws ec2 run-instances --image-id ami-0c94855ba95c71c99 --count 1 --instance-type t2.micro --key-name my-key-pair --security-group-ids sg-0d29a6b892e6a77a3 --subnet-id subnet-0bf25b18d43b73951
+subnet_id=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$vpc_id Name=availabilityZone,Values=us-east-1a)
+echo $subnet_id
+security_group_id="1234"
+#aws ec2 run-instances --image-id ami-02f3f602d23f1659d --count 1 --instance-type t3.micro --security-group-ids $security_group_id --subnet-id $subnet_id

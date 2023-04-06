e8f9be9bbc1b9545f80447af5c6507a58708a3ff
Henrylle Maia <henryllemaia@gmail.com>
2023-04-05 21:50:05 -0300

    pronto para lancar ec2

--- scripts/lancar_ec2_zona_a.sh
+++ scripts/lancar_ec2_zona_a.sh
@@ -1,6 +1,4 @@
 vpc_id=$(aws ec2 describe-vpcs --filters Name=isDefault,Values=true --query "Vpcs[0].VpcId" --output text)
 subnet_id=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$vpc_id Name=availabilityZone,Values=us-east-1a --query "Subnets[0].SubnetId" --output text)
-aws ec2 describe-subnets --filters Name=availabilityZone,Values=us-east-1a --query "Subnets[0].SubnetId" --output text
-echo $subnet_id
-security_group_id="1234"
-#aws ec2 run-instances --image-id ami-02f3f602d23f1659d --count 1 --instance-type t3.micro --security-group-ids $security_group_id --subnet-id $subnet_id
+security_group_id=$(aws ec2 describe-security-groups --group-names "bia-local" --query "SecurityGroups[0].GroupId" --output text)
+aws ec2 run-instances --image-id ami-02f3f602d23f1659d --count 1 --instance-type t3.micro --security-group-ids $security_group_id --subnet-id $subnet_id

843513137a821ecde34331a0ea6ae3e87a60238f
Henrylle Maia <henryllemaia@gmail.com>
2023-04-05 21:15:52 -0300

    Ajustando datas do evento

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 const About = () => {
   return (
     <div>
-      <h4>Versão 1.1.3</h4>
-      <h5>BIA 13 a 19 de Março/2023</h5>
+      <h4>Versão 2.0.0</h4>
+      <h5>BIA 08 a 14 de Maio/2023</h5>
       <Link to="/">Voltar</Link>
     </div>
   );

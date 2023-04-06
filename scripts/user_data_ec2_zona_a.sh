237c209bb2ddaae7bb7c40977d5f11d4cd47dc80
Henrylle Maia <henryllemaia@gmail.com>
2023-04-05 22:19:30 -0300

    .

---
+++ scripts/user_data_ec2_zona_a.sh
@@ -0,0 +1,17 @@
+#Instalar Docker e Git
+sudo yum update -y
+sudo yum install git -y
+sudo yum install docker -y
+sudo usermod -a -G docker ec2-user
+id ec2-user
+newgrp docker
+
+
+#Instalar docker-compose
+wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
+sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+sudo chmod -v +x /usr/local/bin/docker-compose
+
+#Ativar docker
+sudo systemctl enable docker.service
+sudo systemctl start docker.service

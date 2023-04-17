b28c498a229b556679df7bde0e8a0daffe45a186
Henrylle Maia <henryllemaia@gmail.com>
2023-04-17 11:14:46 -0300

    mudançcas na role para principal ser arquivo

--- scripts/criar_role_ssm.sh
+++ scripts/criar_role_ssm.sh
@@ -1,16 +1,6 @@
 role_name="role-acesso-ssm"
 policy_name="AmazonSSMManagedInstanceCore"
 
-aws iam create-role --role-name $role_name --assume-role-policy-document \
-'{
-  "Version": "2012-10-17",
-  "Statement": {
-    "Effect": "Allow",
-    "Principal": {
-      "Service": "ec2.amazonaws.com"
-    },
-    "Action": "sts:AssumeRole"
-  }
-}'
+aws iam create-role --role-name $role_name --assume-role-policy-document file://ec2_principal.json
 
 aws iam attach-role-policy --role-name $role_name --policy-arn arn:aws:iam::aws:policy/$policy_name

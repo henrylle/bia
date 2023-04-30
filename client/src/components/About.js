5262724cb034b99a28a300766304e88c2a82af41
Henrylle Maia <henryllemaia@gmail.com>
2023-04-30 18:15:43 -0300

    Update About.js

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,7 +3,7 @@ const About = () => {
   return (
     <div>
       <h4>Versão 2.0.0</h4>
-      <h5>BIA 08 a 14 de Maio/2023</h5>
+      <h5>BIA 15 a 21 de Maio/2023</h5>
       <Link to="/">Voltar</Link>
     </div>
   );

a76f14f0faef03b52865dc36d6714dc41d022f9b
Henrylle Maia <henryllemaia@gmail.com>
2023-05-15 11:13:43 -0300

    ajuste na permissao + recebendo id da instancia

--- scripts/start-session-bash.sh
+++ scripts/start-session-bash.sh
@@ -1,1 +1,3 @@
-aws ssm start-session --target INSTANCE_ID --document-name AWS-StartInteractiveCommand --parameters command="bash -l"
+INSTANCE_ID=$1
+echo $INSTANCE_ID
+#aws ssm start-session --target $INSTANCE_ID --document-name AWS-StartInteractiveCommand --parameters command="bash -l"

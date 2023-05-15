0ac6b23505d62f6a618b35a8f6e57c2aaa3dddbf
Henrylle Maia <henryllemaia@gmail.com>
2023-05-15 11:18:38 -0300

    ajuste no script

--- scripts/start-session-bash.sh
+++ scripts/start-session-bash.sh
@@ -1,3 +1,3 @@
 INSTANCE_ID=$1
-echo $INSTANCE_ID
-#aws ssm start-session --target $INSTANCE_ID --document-name AWS-StartInteractiveCommand --parameters command="bash -l"
+echo "Conectando na instancia $INSTANCE_ID"
+aws ssm start-session --target $INSTANCE_ID --document-name AWS-StartInteractiveCommand --parameters command="bash -l"

2f54ec5aa9dadffbcaa4c3f1cc1259c09c3073c7
Henrylle Maia <henryllemaia@gmail.com>
2023-12-06 23:19:13 -0300

    mudança importante para suportar pg >15

--- config/database.js
+++ config/database.js
@@ -5,4 +5,25 @@ module.exports = {
   host: process.env.DB_HOST || "127.0.0.1",
   port: process.env.DB_PORT || 5433,
   dialect: "postgres",
+  dialectOptions: isLocalConnection() ? {} : getRemoteDialectOptions(),
 };
+
+function isLocalConnection() {
+  // Lógica para determinar se a conexão é local
+  return (
+    process.env.DB_HOST === undefined ||
+    process.env.DB_HOST === "database" ||
+    process.env.DB_HOST === "127.0.0.1" ||
+    process.env.DB_HOST === "localhost"
+  );
+}
+
+function getRemoteDialectOptions() {
+  // Configurações específicas para conexões remotas (útil a partir do pg 15)
+  return {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  };
+}

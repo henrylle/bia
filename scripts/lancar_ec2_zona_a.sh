f7e928b4ebb9df2a7626f19cfef8fa6caef4fe86
Henrylle Maia <henryllemaia@gmail.com>
2024-01-04 21:38:14 -0300

    Associando ip publico ao lancar ec2.

--- scripts/lancar_ec2_zona_a.sh
+++ scripts/lancar_ec2_zona_a.sh
@@ -3,7 +3,7 @@ subnet_id=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$vpc_id Name=a
 security_group_id=$(aws ec2 describe-security-groups --group-names "bia-dev" --query "SecurityGroups[0].GroupId" --output text)
 
 aws ec2 run-instances --image-id ami-02f3f602d23f1659d --count 1 --instance-type t3.micro \
---security-group-ids $security_group_id --subnet-id $subnet_id \
+--security-group-ids $security_group_id --subnet-id $subnet_id --associate-public-ip-address \
 --block-device-mappings '[{"DeviceName":"/dev/xvda","Ebs":{"VolumeSize":15,"VolumeType":"gp2"}}]' \
 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=bia-dev}]' \
 --iam-instance-profile Name=role-acesso-ssm --user-data file://user_data_ec2_zona_a.sh

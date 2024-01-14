00ef387940b271d421b030e7857e593c674d12ec
Henrylle Maia <henryllemaia@gmail.com>
2024-01-14 13:59:00 -0300

    suprimindo erro

--- scripts/lancar_ec2_zona_a.sh
+++ scripts/lancar_ec2_zona_a.sh
@@ -1,6 +1,6 @@
 vpc_id=$(aws ec2 describe-vpcs --filters Name=isDefault,Values=true --query "Vpcs[0].VpcId" --output text)
 subnet_id=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$vpc_id Name=availabilityZone,Values=us-east-1a --query "Subnets[0].SubnetId" --output text)
-security_group_id=$(aws ec2 describe-security-groups --group-names "bia-dev" --query "SecurityGroups[0].GroupId" --output text)
+security_group_id=$(aws ec2 describe-security-groups --group-names "bia-dev" --query "SecurityGroups[0].GroupId" --output text 2>/dev/null)
 
 if [ -z "$security_group_id" ]; then
     echo ">[ERRO] Security group bia-dev não foi criado na VPC $vpc_id"

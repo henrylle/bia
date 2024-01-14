778171354f05bf72c5031e4b7f00d9d1b9a91eca
Henrylle Maia <henryllemaia@gmail.com>
2024-01-14 13:32:41 -0300

    Verificando se a rola iam ja foi criada previamente.

--- scripts/criar_role_ssm.sh
+++ scripts/criar_role_ssm.sh
@@ -1,6 +1,11 @@
 role_name="role-acesso-ssm"
 policy_name="AmazonSSMManagedInstanceCore"
 
+if aws iam get-role --role-name "$role_name" &> /dev/null; then
+    echo "A IAM role $role_name já existe."
+    exit 1
+fi
+
 aws iam create-role --role-name $role_name --assume-role-policy-document file://ec2_principal.json
 # Cria o perfil de instância
 aws iam create-instance-profile --instance-profile-name $role_name

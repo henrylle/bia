f91a13e10645e125af20c3682306abbae189ee17
Henrylle Maia <henryllemaia@gmail.com>
2024-01-14 10:46:32 -0300

    Mudanças para validar melhor a configuração do security group.

--- scripts/validar_recursos_zona_a.sh
+++ scripts/validar_recursos_zona_a.sh
@@ -2,25 +2,43 @@ vpc_id=$(aws ec2 describe-vpcs --filters Name=isDefault,Values=true --query "Vpc
 if [ $? -eq 0 ]; then
   echo "[OK] Tudo certo com a VPC"
 else
-  echo "[ERRO] Tenho um problema ao retornar a VPC default. Será se ela existe?"
+  echo ">[ERRO] Tenho um problema ao retornar a VPC default. Será se ela existe?"
 fi
 
 subnet_id=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$vpc_id Name=availabilityZone,Values=us-east-1a --query "Subnets[0].SubnetId" --output text 2>/dev/null)
 if [ $? -eq 0 ]; then
   echo "[OK] Tudo certo com a Subnet"
 else
-  echo "[ERRO] Tenho um problema ao retornar a subnet da zona a. Será se existe uma subnet na zona A?"
+  echo ">[ERRO] Tenho um problema ao retornar a subnet da zona a. Será se existe uma subnet na zona A?"
 fi
 
 security_group_id=$(aws ec2 describe-security-groups --group-names "bia-dev" --query "SecurityGroups[0].GroupId" --output text 2>/dev/null)
 if [ $? -eq 0 ]; then
-  echo "[OK] Tudo certo com o Security Group"
+  echo "[OK] Security Group bia-dev foi criado"
+  
+  # Validar inbound rule para o security group 'bia-dev'
+  inbound_rule=$(aws ec2 describe-security-groups --group-ids $security_group_id --query "SecurityGroups[0].IpPermissions[?ToPort=='3001' && IpProtocol=='tcp' && IpRanges[0].CidrIp=='0.0.0.0/0']" --output text)
+  
+  if [ -n "$inbound_rule" ]; then
+    echo " [OK] Regra de entrada para porta 3001 está aberta para o mundo todo."
+  else
+    echo " >[ERRO] Regra de entrada para porta 3001 não encontrada ou não está aberta para o mundo todo. Reveja a aula do Henrylle."
+  fi
+
+  # Validar outbound rule para o security group 'bia-dev'
+  outobund_rule=$(aws ec2 describe-security-groups --group-ids $security_group_id --query "SecurityGroups[0].IpPermissionsEgress[?IpProtocol=='-1' && IpRanges[0].CidrIp=='0.0.0.0/0']" --output text)
+  
+  if [ -n "$outobund_rule" ]; then
+    echo " [OK] Regra de saída para o mundo todo está correta."
+  else
+    echo " >[ERRO] Regra de saída para o mundo todo não encontrada. Reveja a aula do Henrylle."
+  fi
 else
-  echo "[ERRO] Não achei o security group bia-dev. Ele foi criado?"
+  echo ">[ERRO] Não achei o security group bia-dev. Ele foi criado?"
 fi
 
 if aws iam get-role --role-name role-acesso-ssm &>/dev/null; then
     echo "[OK] Tudo certo com a role 'role-acesso-ssm'"
 else
-    echo "[ERRO] A role 'role-acesso-ssm' não existe"
+    echo ">[ERRO] A role 'role-acesso-ssm' não existe"
 fi

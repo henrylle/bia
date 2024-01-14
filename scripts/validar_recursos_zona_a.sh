7afcba404468c49d0c49516397f03ab9f4e0a465
Henrylle Maia <henryllemaia@gmail.com>
2024-01-14 11:32:58 -0300

    aprimorando checagem de regra de entrada

--- scripts/validar_recursos_zona_a.sh
+++ scripts/validar_recursos_zona_a.sh
@@ -17,21 +17,21 @@ if [ $? -eq 0 ]; then
   echo "[OK] Security Group bia-dev foi criado"
   
   # Validar inbound rule para o security group 'bia-dev'
-  inbound_rule=$(aws ec2 describe-security-groups --group-ids $security_group_id --query "SecurityGroups[0].IpPermissions[?ToPort=='3001' && IpProtocol=='tcp' && IpRanges[0].CidrIp=='0.0.0.0/0']" --output text)
-  
+  inbound_rule=$(aws ec2 describe-security-groups --group-ids $security_group_id --filters "Name=ip-permission.from-port,Values=3001" --filters "Name=ip-permission.cidr,Values=0.0.0.0/0" --output text)
+
   if [ -n "$inbound_rule" ]; then
-    echo " [OK] Regra de entrada para a porta 3001 está aberta para o mundo todo."
+    echo " [OK] Regra de entrada está ok"
   else
-    echo " >[ERRO] Regra de entrada para a porta 3001 não encontrada ou não está aberta para o mundo todo. Reveja a aula do Henrylle."
+    echo " >[ERRO] Regra de entrada para a porta 3001 não encontrada ou não está aberta para o mundo todo. Reveja a aula do Henrylle"
   fi
 
   # Validar outbound rule para o security group 'bia-dev'
   outobund_rule=$(aws ec2 describe-security-groups --group-ids $security_group_id --query "SecurityGroups[0].IpPermissionsEgress[?IpProtocol=='-1' && IpRanges[0].CidrIp=='0.0.0.0/0']" --output text)
   
   if [ -n "$outobund_rule" ]; then
-    echo " [OK] Regra de saída para o mundo todo está correta."
+    echo " [OK] Regra de saída está correta"
   else
-    echo " >[ERRO] Regra de saída para o mundo todo não encontrada. Reveja a aula do Henrylle."
+    echo " >[ERRO] Regra de saída para o mundo não encontrada. Reveja a aula do Henrylle"
   fi
 else
   echo ">[ERRO] Não achei o security group bia-dev. Ele foi criado?"

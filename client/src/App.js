cbfb8e1ce24ea751b8f982c1dd27518109fc548d
aluno <aluno@pop-os.localdomain>
2024-05-10 01:03:04 -0300

    Changinfg app.js

--- client/src/App.js
+++ client/src/App.js
@@ -100,7 +100,7 @@ function App() {
                   onToggle={toggleReminder}
                 />
               ) : (
-                "Nenhuma tarefa nesse momento"
+                "No tasks so far"
               )}
             </>
           )}

309e3bc1732e5a27fb3108d646694976dc6c483f
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2024-05-10 12:00:18 -0300

    alteracao pequena para push deploy

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -4,7 +4,7 @@ import DadosHenrylle from "./DadosHenrylle";
 const About = () => {
   return (
     <div>
-      <h4>Versão 2.1.0</h4>
+      <h4>Versão 2.2.0</h4>
       <h5>BIA 18 a 24 de Março/2024</h5>
       <Link to="/">Voltar</Link>
       <DadosHenrylle />

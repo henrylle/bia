d0daf9954f1fc89cc09dbc7f990df8508d5f5d81
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2024-05-13 11:28:54 -0300

    Alterando a versão da Bia para o correto

--- api/controllers/versao.js
+++ api/controllers/versao.js
@@ -2,7 +2,7 @@ module.exports = () => {
   const controller = {};
 
   controller.get = (req, res) => {
-    const responseString = `Bia ${process.env.VERSAO_API || "2.0.2"}`;
+    const responseString = `Bia ${process.env.VERSAO_API || "2.0.0"}`;
     res.send(responseString);
   };
   return controller;

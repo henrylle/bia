0f2594eb54f8a779b07ca8454ca7c6ca5f280335
Edgar Galindo de Moura <edgar.edps@gmail.com>
2024-05-15 10:20:09 -0300

    mudando botões para português

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,8 +10,8 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "red" : "black"}
-          text={showAdd ? "Close" : "Add"}
+          color={showAdd ? "yellow" : "green"}
+          text={showAdd ? "Fechar" : "Adicionar"}
           onClick={onAdd}
         />
       )}

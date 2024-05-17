8014aebff1f6d354662ddbf20eb46dc33b3ade01
aluno <aluno@pop-os.localdomain>
2024-05-17 00:26:52 -0300

    Updating bia version

--- api/controllers/versao.js
+++ api/controllers/versao.js
@@ -2,7 +2,7 @@ module.exports = () => {
   const controller = {};
 
   controller.get = (req, res) => {
-    const responseString = `Bia ${process.env.VERSAO_API || "2.0.0"}`;
+    const responseString = `Bia ${process.env.VERSAO_API || "2.0.1"}`;
     res.send(responseString);
   };
   return controller;

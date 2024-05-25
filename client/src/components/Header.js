4629b3623cd1caef91c2d5aee4bdcc0dbc893b8a
Edgar Galindo de Moura <edgar.edps@gmail.com>
2024-05-25 11:28:32 -0300

    mudanca na msg da bia

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -20,7 +20,7 @@ const Header = ({ title, onAdd, showAdd }) => {
 };
 
 Header.defaultProps = {
-  title: "BIA 2024 - ECS CLUSTER-BIA-PIPELINE 2024",
+  title: "BIA 2024 - ECS CLUSTER-PIPELINE DA BIA 2024",
 };
 
 export default Header;

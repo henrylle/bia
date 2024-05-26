7a32acb2169b740f4a6e51c00683cf922ef55f5d
Henrylle Maia <henryllemaia@gmail.com>
2024-05-26 14:49:05 -0300

    ajuste no database para secrets + debug

--- api/controllers/versao.js
+++ api/controllers/versao.js
@@ -1,9 +1,10 @@
 module.exports = () => {
   const controller = {};
 
-  controller.get = (req, res) => {
+  controller.get = async (req, res) => {
     const responseString = `Bia ${process.env.VERSAO_API || "2.0.0"}`;
     res.send(responseString);
   };
+
   return controller;
 };

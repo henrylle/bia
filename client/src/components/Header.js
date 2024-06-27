c5664f32eb29b3441b4a543e74a644909719511f
Henrylle Maia <henryllemaia@gmail.com>
2024-06-27 18:04:07 -0300

    mudando cor do botao

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "red" : "black"}
+          color={showAdd ? "blue" : "green"}
           text={showAdd ? "Close" : "Add"}
           onClick={onAdd}
         />

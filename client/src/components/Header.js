c74ddfe37e10db9659d5662a9da93c052a005318
Henrylle Maia <henryllemaia@gmail.com>
2024-06-27 20:24:58 -0300

    deploy na aula magna

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,8 +10,8 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "blue" : "green"}
-          text={showAdd ? "Close" : "Add"}
+          color={showAdd ? "red" : "blue"}
+          text={showAdd ? "Fechar" : "Adicionar"}
           onClick={onAdd}
         />
       )}

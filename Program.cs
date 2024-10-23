37f897bd47b960f71bad47fd1bb416e708d872de
Leonardo <leoguaruleo@gmail.com>
2024-10-23 19:45:27 -0300

    Ajuste de Migrations

--- Program.cs
+++ Program.cs
@@ -1,14 +1,6 @@
 using BIA.Data;
 using BIA.Services;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Threading;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,29 +8,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<MeuDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddTransient<IServiceApi, ServiceApi>();
 var app = builder.Build();
-using (var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
-{
-    var logger = serviceScope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-    var db = serviceScope.ServiceProvider.GetRequiredService<MeuDbContext>().Database;
-
-    logger.LogInformation("Migrando banco de dados...");
-
-    while (!db.CanConnect())
-    {
-        logger.LogInformation("Banco de dados ainda não pronto; esperando...");
-        Thread.Sleep(1000);
-    }
-
-    try
-    {
-        serviceScope.ServiceProvider.GetRequiredService<MeuDbContext>().Database.Migrate();
-        logger.LogInformation("Banco de dados migrado com sucesso.");
-    }
-    catch (Exception ex)
-    {
-        logger.LogError(ex, "Ocorreu um erro ao migrar o banco de dados.");
-    }
-}
+await MigrationExtensions.AplicarMigrationsAsync(app);
 
 if (!app.Environment.IsDevelopment())
 {

27b32a62615726ff8700ae0639cbd6ddddbfe02f
Henrylle Maia <henryllemaia@gmail.com>
2024-11-01 15:05:26 -0300

    ajustes na bia para evolucao do projeto

--- config/express.js
+++ config/express.js
@@ -1,33 +1,26 @@
 const express = require("express");
-const cors = require("cors");
-const path = require("path");
+var cors = require("cors");
+var path = require("path");
 const config = require("config");
-const bodyParser = require("body-parser");
+var bodyParser = require("body-parser");
 
-const initializeExpress = async () => {
-  try {
-    const app = express();
+module.exports = () => {
+  const app = express();
 
-    // SETANDO VARIÁVEIS DA APLICAÇÃO
-    app.set("port", process.env.PORT || config.get("server.port"));
+  // SETANDO VARIÁVEIS DA APLICAÇÃO
+  app.set("port", process.env.PORT || config.get("server.port"));
 
-    // Setando react
-    app.use(express.static(path.join(__dirname, "../", "client", "build")));
+  //Setando react
+  app.use(express.static(path.join(__dirname, "../", "client", "build")));
 
-    // parse request bodies (req.body)
-    app.use(express.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
+  // parse request bodies (req.body)
+  app.use(express.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
 
-    app.use(cors());
+  app.use(cors());
 
-    require("../api/routes/versao")(app);
-    require("../api/routes/tarefas")(app);    
+  require("../api/routes/tarefas")(app);
+  require("../api/routes/versao")(app);
 
-    return app;
-  } catch (error) {
-    console.error("Erro ao inicializar o Express:", error);
-    throw error;
-  }
+  return app;
 };
-
-module.exports = initializeExpress;

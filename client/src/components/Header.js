eb991521ba4da5330561e68f2bd4fe40bada2379
leosimao88br <leonardo.simao1988@gmail.com>
2024-11-13 14:28:06 -0300

    cor verde

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "red" : "blue"}
+          color={showAdd ? "green" : "blue"}
           text={showAdd ? "Fechar" : "Adicionar"}
           onClick={onAdd}
         />

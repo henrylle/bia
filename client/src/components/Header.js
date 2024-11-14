1cd8cdc32e0a8535d89392e9d70806c039c989b2
stalin velchael mendez <115166348+Velchael@users.noreply.github.com>
2024-11-13 21:00:14 -0300

    mudando o header para ingles

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,8 +10,8 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "red" : "blue"}
-          text={showAdd ? "Fechar" : "Adicionar"}
+          color={showAdd ? "blue" : "red"}
+          text={showAdd ? "Closee" : "Addd"}
           onClick={onAdd}
         />
       )}

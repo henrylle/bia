ede7b068ecc64f9a8effff9c94fe2b28b0445225
giumk <giullyanmk@hotmail.com>
2024-12-01 21:13:12 -0300

    Atualizando versão da bia

--- api/controllers/versao.js
+++ api/controllers/versao.js
@@ -2,7 +2,7 @@ module.exports = () => {
   const controller = {};
 
   controller.get = async (req, res) => {
-    const responseString = `Bia ${process.env.VERSAO_API || "2.0.0"}`;
+    const responseString = `Bia ${process.env.VERSAO_API || "2.0.1"}`;
     res.send(responseString);
   };
 

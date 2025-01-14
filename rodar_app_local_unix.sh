a3d881aa397758e49c9b097a056750c56e60d2c1
EdvaldoJose <edvaldojose003@gmail.com>
2025-01-14 20:06:38 -0300

    Correcao do arquivo rodar_app_local_unix.sh

--- rodar_app_local_unix.sh
+++ rodar_app_local_unix.sh
@@ -1,6 +1,6 @@
-docker compose up -d database
+#docker compose up -d database
 npm install --loglevel=error
 npm run build --prefix client --loglevel=error
-npx sequelize db:migrate
+#npx sequelize db:migrate
 docker compose up -d
 #npm start

16c36f6db101a20d1e43a863aa308514778bf512
ailsonpacheco <pacheco08@gmail.com>
2025-06-15 03:17:50 -0300

    Ajuste Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "red" : "blue"}
+          color={showAdd ? "black" : "green"}
           text={showAdd ? "Fechar" : "Adicionar"}
           onClick={onAdd}
         />
@@ -20,7 +20,7 @@ const Header = ({ title, onAdd, showAdd }) => {
 };
 
 Header.defaultProps = {
-  title: "BIA 2025 ! ",
+  title: "BIA 2025",
 };
 
 export default Header;

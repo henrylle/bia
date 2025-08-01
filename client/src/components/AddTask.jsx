b611c3b6981925ce90ecd2f48291d64dd9f226e9
Henrylle Maia <henrymaiabia@gmail.com>
2025-08-01 17:29:30 +0000

    ajuste no dockerfile

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Add Taskeris
+        Adicionar Task
       </button>
       
       <Modal

566db4df79c091c6e08820fdacb6c317939a4f6c
Henrylle Maia <henrymaiabia@gmail.com>
2025-08-01 23:26:12 +0000

    alterando texto do botao

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Adicionar Task
+        Adicionar Tarefa
       </button>
       
       <Modal

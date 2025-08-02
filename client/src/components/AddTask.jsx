da50a2002a632dee4c5b1dadfee8611a73b6b470
Henrylle Maia <henrymaiabia@gmail.com>
2025-08-02 00:39:02 +0000

    disparando primeiro pipeline com push

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Adicionar Tarefa
+        Add New Task
       </button>
       
       <Modal

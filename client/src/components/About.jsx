e5fd813f7ecf4243eb8f6b0b582f8dca8a70062b
Henrylle Maia <henryllemaia@gmail.com>
2026-01-22 16:49:21 -0300

    ajuste no projeto da bia

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
           <div className="feature-card highlight">
             <h3>Próximo Evento</h3>
             <h4>AWS & IA</h4>
-            <p><strong>27/09 e 28/09/2025</strong><br/>Formação AWS</p>
+            <p><strong>31/01 e 01/02/2026</strong><br/>Formação AWS</p>
           </div>
         </div>
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -24,7 +24,7 @@ const Header = ({ title }) => {
 };
 
 Header.defaultProps = {
-  title: "BIA 2025",
+  title: "BIA 2026",
 };
 
 export default Header;
